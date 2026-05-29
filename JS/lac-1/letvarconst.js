294a6f7451d433261d5cc4e52a65022a6be7d6d4
unknown <sanubosestp90@gmail.com>
2026-05-29 22:06:42 +0530

    there lacture of JS

---
+++ JS/lac-1/letvarconst.js
@@ -0,0 +1,19 @@
+// var num =50;
+// function print(){
+//     var square = num*num;
+//     console.log(square)
+// }
+// console.log(num)
+// print();
+//var num =50;
+// function print(){
+//     var num =50;
+//     var square = num*num;
+//     console.log(square)
+// }
+// console.log(num)
+// print();
+
+console.log(num);
+var num =60;
+console.log(num)
---
+++ JS/lac-1/operation.js
@@ -0,0 +1,31 @@
+//An operand is the value or variable on which an operator performs an operation.
+// console.log(2+5);
+// let age = 18;
+// console.log(age+18);
+// let x=8; 
+// let y =5;
+// console.log(x-y);
+// console.log(x+y);
+// console.log(x/y);
+// console.log(x**y);
+// console.log(x*y);
+// console.log(x%y);
+
+// let name= "sanu"
+// let lastname ="bose"
+// console.log(name+ "  " +lastname);
+// console.log(name-lastname);
+
+
+
+// console.log(9==9);
+// console.log(9!=20);
+// console.log(2>10);
+// console.log(2<10);
+// console.log(2>=10);
+// console.log(2<=10); // less than equal too
+// console.log(10<=10);
+
+
+console.log("9"==9);// true
+console.log("9"===9);//false
