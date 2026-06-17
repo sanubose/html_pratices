8af05d85810dcd11ca9117bccbb36d6e883496eb
unknown <sanubosestp90@gmail.com>
2026-06-17 13:17:31 +0530

    loops

---
+++ JS/LOOPs/loo.js
@@ -0,0 +1,18 @@
+for(let i =1; i<=28; i*=3){
+    console.log("aarti",i)
+}
+
+for(let i =18; i>=1; i--){
+    console.log("aarti",i)
+}
+
+let count =0;
+for(let i =0; i<5; i++){
+    console.log("count" ,i)
+}
+
+let i =0 
+while(i<5){
+    console.log("i->",i)
+    i++
+}
