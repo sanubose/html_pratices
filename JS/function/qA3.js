072c4d902244a4f8bd04f182d26b0a00cfe06cc4
unknown <sanubosestp90@gmail.com>
2026-06-17 13:20:55 +0530

    here arr

---
+++ JS/function/qA3.js
@@ -0,0 +1,13 @@
+const multiple = (arr)=>{
+    let ans = [];
+    for(let i=0; i<arr.length; i++){
+        ans.push(arr[i]*2)
+    }
+    return ans
+}
+console.log(multiple([1,2,3,4,5]))
+
+
+
+
+ 
